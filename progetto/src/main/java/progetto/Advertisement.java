aa7965e7d8be7f8021b3a5200b447dbfeb544e6d
armando.laplaca <armando.laplaca@gmail.com>
2019-07-11 20:41:21 +0200

    modifiche

--- progetto/src/main/java/progetto/Advertisement.java
+++ progetto/src/main/java/progetto/Advertisement.java
@@ -3,6 +3,7 @@ package progetto;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -12,6 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +21,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.graphx.Edge;
 import org.apache.spark.graphx.EdgeDirection;
 import org.apache.spark.graphx.EdgeRDD;
@@ -32,18 +35,22 @@ import scala.reflect.ClassTag;
 
 public class Advertisement {
 	/* Parametri di configurazione */
-	public static String numeroCore = "1"; /* Numero di core per l'esecuzione */
-	public static Integer tipologiaGrafo = 1; /* Dimensione del grafo */
-	public static Double soglia = .8; /* Soglia di accettazione */
-	/* Strutture di supporto */
+	public static String numeroCore = "4"; /* Numero di core per l'esecuzione */
+	public static Integer tipologiaGrafo = 3; /* Dimensione del grafo */
+	public static Double soglia = .5; /* Soglia di accettazione */
+	public static int k = 10; /* Numero di nodi da cercare */
+	/* Altre variabili */
 	public final static Double INFINITY = Double.MAX_VALUE;
 	public static JavaSparkContext jsc;
-	public static Graph<Long, Long> grafo;
 	public static FileWriter fw;
+	/* Strutture di supporto */
+	public static Graph<Long, Long> grafo;
+	public static List<Integer> vertici_archi = new ArrayList<Integer>(); /* 0: Numero vertici, 1: Numero archi */
 	public static Map<Long, Double> mappaAffinita = new HashMap<Long, Double>();
 	public static VertexRDD<long[]> mappaVicini;
 	public static JavaPairRDD<Long, Double> mappaUtilita;
 	public static Map<Integer, String> mappaFile = new HashMap<Integer, String>();
+	public static List<Long> listaVertici = new ArrayList<Long>();
 
 	/**
 	 * Funzione per il caricamento di un grafo a partire da un file con path passato
@@ -60,7 +67,10 @@ public class Advertisement {
 		 * Caricamento del grafo a partire dal file passato a parametro.
 		 */
 		System.out.println("Inizio lettura grafo da file");
-		JavaRDD<String> file = jsc.textFile(path);
+		JavaRDD<String> file = jsc.textFile(path).filter(f -> !f.startsWith("#"));
+		/* Leggo il numero di vertici e archi dal file, nelle righe con "#" */
+		JavaRDD<String> header = jsc.textFile(path).filter(f -> f.startsWith("#"));
+		header.collect().stream().forEach(e -> vertici_archi.add(Integer.parseInt(e.replace("#", ""))));
 		JavaRDD<Edge<Long>> archi = file.map(f -> {
 			if (f != null) {
 				String[] vertici = f.split(" "); // ^([0-9]+)
@@ -149,8 +159,7 @@ public class Advertisement {
 			 */
 			for (Tuple2<Object, long[]> tupla : mappaVicini.toJavaRDD().collect()) {
 				vertice_src = (Long) tupla._1();
-				valore_src = random
-						.nextDouble(); /* TODO: mettere un livello massimo di affinità ed evitare valori troppo alti */
+				valore_src = random.nextDouble() * .79;
 				if (!inseriti.contains(vertice_src)) {
 					inseriti.add(vertice_src);
 					fileText = vertice_src + " " + valore_src + "\n";
@@ -161,12 +170,11 @@ public class Advertisement {
 				 * essi ripeto la procedura di inserimento su file, controllando che non siano
 				 * già stati inseriti nell'insieme di vertici già valutati.
 				 */
-				/*
-				 * TODO: considerare il 20-30% di questi con affinità vicina al nodo sorgente,
-				 * mentre i restanti devono avere affinità molto lontana
-				 */
-				for (int i = 0; i < tupla._2().length; i++) {
-					vertice_adj = tupla._2()[i];
+				List<Long> vicini = new ArrayList<>();
+				Arrays.stream(tupla._2()).forEach(element -> vicini.add(element));
+				Collections.shuffle(vicini);
+				for (int i = 0; i < vicini.size(); i++) {
+					vertice_adj = vicini.get(i);
 					if (!inseriti.contains(vertice_adj)) {
 						inseriti.add(vertice_adj);
 						/*
@@ -174,8 +182,11 @@ public class Advertisement {
 						 * principale, sommando o sottraendo un valore casuale preso in percentuale al
 						 * minimo valore tra 0.2 o (1-valore_src), per evitare di andare oltre l'1.
 						 */
-						valore_adj = valore_src + ((random.nextBoolean() ? 1 : -1)
-								* (Math.min(1 - valore_src, random.nextDouble() * 0.2)) * random.nextDouble());
+						if (i < vicini.size() * .3) {
+							valore_adj = valore_src + ((random.nextBoolean() ? 1 : -1) * random.nextDouble() * 0.2);
+						} else {
+							valore_adj = valore_src + (-1 * random.nextDouble() * (valore_src * .7));
+						}
 						fileText = vertice_adj + " " + valore_adj + "\n";
 						fw.write(fileText);
 					}
@@ -282,66 +293,153 @@ public class Advertisement {
 	}
 
 	/**
-	 * Funzione per la stampa dei valori che superano una certa soglia.
+	 * Funzione per la stampa e la conta dei nodi le cui affinità che superano una
+	 * certa soglia. Tra i nodi coinvolti consideriamo anche i nodi adiacenti.
 	 * 
 	 * @param listaVertici Lista ordinata di vertici da stampare.
 	 */
-	public static void stampaNodi(List<Tuple2<Long, Double>> listaVertici) {
-		int i = 1;
+	public static long contaNodi(List<Tuple2<Long, Double>> listaVertici) {
+		/* Insieme di vertici non ripetuti */
+		Set<Long> inseriti = new HashSet<Long>();
+		/*
+		 * Memorizza in hash le liste di adiacenze
+		 */
+		Map<Long, long[]> hashVicini = mappaVicini.toJavaRDD().mapToPair(s -> {
+			return new Tuple2<Long, long[]>((Long) s._1(), s._2());
+		}).collectAsMap();
+		long i = 1, risultato = 0;
+		/*
+		 * Scorro tutti gli elementi della lista passata a parametro
+		 */
 		for (Tuple2<Long, Double> vertice : listaVertici) {
-			if (vertice._2() < soglia) {
-				System.out.println("Non ci sono più elementi sopra la soglia!");
-				break;
+			/*
+			 * Se l'elemento supera la soglia, lo conto e stampo, il valore di affinità
+			 * altrimenti stampo un messaggio di notifica.
+			 */
+			if (vertice._2() >= soglia && !inseriti.contains(vertice._1())) {
+				inseriti.add(vertice._1());
+				System.out.println("\t" + i + ") " + vertice._1() + ": " + vertice._2());
+				risultato++;
 			} else {
-				System.out.println(i + ") " + vertice._1() + ": " + vertice._2());
-				i++;
+				System.out.println("\t" + i + ") " + vertice._1() + ": Soglia non superata!");
 			}
+			/* Controllo se l'affinità dei suoi vicini è maggiore della soglia */
+			long[] vicini = hashVicini.get(vertice._1());
+			for (int j = 0; j < vicini.length; j++) {
+				/*
+				 * Stampo e conto solamente gli elementi la cui affinità supera la soglia
+				 */
+				if (mappaAffinita.get(vicini[j]) >= soglia && !inseriti.contains(vicini[j])) {
+					inseriti.add(vicini[j]);
+					System.out.println("\t\t" + vicini[j] + ": " + mappaAffinita.get(vicini[j]));
+					risultato++;
+				}
+			}
+			i++;
 		}
+		return risultato;
 	}
 
 	public static void main(String[] args) {
 		/* Configurazione di Spark e dei file */
-		mappaFile.put(1, "grande-abbastanza.txt");
-		mappaFile.put(2, "grande.txt");
-		mappaFile.put(3, "medio.txt");
-		mappaFile.put(4, "piccolo.txt");
-
-		System.setProperty("hadoop.home.dir", "C:\\Hadoop");
-		SparkConf conf = new SparkConf().setAppName("Advertisement").setMaster("local[*]")
-				.set("spark.driver.cores", numeroCore).set("spark.driver.memory", "4g");
-		jsc = new JavaSparkContext(conf);
+		configuraParametri();
 		/* Caricamento del grafo in memoria */
-		caricaGrafo("src/main/resources/grafo-" + mappaFile.get(tipologiaGrafo), false);
-		GraphOps<Long, Long> graphOps = Graph.graphToGraphOps(grafo, grafo.vertices().vdTag(),
-				grafo.vertices().vdTag());
-		long numVertici = graphOps.numVertices(); /* Numero vertici usato per la stampa dei tempi */
-		long numEdge = graphOps.numEdges(); /* Numero di archi usato per la stampa dei tempi */
-
+		caricaGrafo("src/main/resources/grafo-" + mappaFile.get(tipologiaGrafo), true);
 		/* Esecuzione */
-		int k = (int) 10;
 		System.out.println("Calcolo dei migliori K");
 		long previousTime = System.currentTimeMillis();
 		/*
 		 * Verifico che il numero di nodi sia maggiore o uguale a k.
 		 */
-		List<Tuple2<Long, Double>> risultato = KMigliori((k <= ((int) numVertici) ? k : ((int) numVertici)));
+		List<Tuple2<Long, Double>> risultato = KMigliori((k <= (vertici_archi.get(0)) ? k : vertici_archi.get(0)));
 		double elapsedTime = (System.currentTimeMillis() - previousTime) / 1000.0;
-		jsc.close();
 
 		/* Stampa dei risultati */
-		try {
-			TimeUnit.SECONDS.sleep(1);
-			System.out.println("Archi: " + numEdge + "\nNodi: " + numVertici + "\nTempo di esecuzione :" + elapsedTime);
-			for (int i = 0; i < 3; i++) {
-				System.out.print(".");
-				TimeUnit.SECONDS.sleep(1);
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		System.out.println("Tempo di esecuzione :" + elapsedTime);
+
+		long affinita, utilita, casuale;
 		System.out.println("Primi " + k + " rispetto ad Affinità");
-		stampaNodi(ordinaPerValore(mappaAffinita).subList(0, (k > mappaAffinita.size() ? mappaAffinita.size() : k)));
+		affinita = contaNodi(
+				ordinaPerValore(mappaAffinita).subList(0, (k > mappaAffinita.size() ? mappaAffinita.size() : k)));
 		System.out.println("Primi " + k + " rispetto ad Utilità");
-		stampaNodi(risultato);
+		utilita = contaNodi(risultato);
+		/*
+		 * Genero una lista di vertici casuali per confrontarla con le altre liste
+		 * ordinate per affinità e utilità
+		 */
+
+		grafo.vertices().toJavaRDD().foreach(new VoidFunction<Tuple2<Object, Long>>() {
+
+			@Override
+			public void call(Tuple2<Object, Long> t) throws Exception {
+				listaVertici.add((Long) t._1());
+			}
+		});
+		Collections.shuffle(listaVertici);
+		List<Tuple2<Long, Double>> listaCasuale = new ArrayList<Tuple2<Long, Double>>();
+		listaVertici.subList(0, k).stream().forEach(elemento -> {
+			listaCasuale.add(new Tuple2<Long, Double>(elemento, mappaAffinita.get(elemento)));
+		});
+		System.out.println("Primi " + k + " rispetto ad Casualità");
+		casuale = contaNodi(listaCasuale);
+		System.out.println("Nodi trovati per affinità: " + affinita);
+		System.out.println("Nodi trovati per utilità: " + utilita);
+		System.out.println("Nodi trovati per lista casuale: " + casuale);
+		jsc.close();
+	}
+
+	private static void configuraParametri() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Inserire tipologia di grafo (Default: 2)");
+		System.out.println("1) Nodi: 6726011\n   Archi: 19360690");
+		System.out.println("2) Nodi: 513969\n   Archi: 3190452");
+		System.out.println("3) Nodi: 4039\n   Archi: 88234");
+		try {
+			tipologiaGrafo = input.nextInt();
+		} catch (Exception e) {
+			System.out.println("** Valore di Default **");
+			tipologiaGrafo = 3;
+			input.nextLine();
+		}
+		System.out.println("Inserire soglia (Default: 0,7)");
+		try {
+			soglia = input.nextDouble();
+		} catch (Exception e) {
+			System.out.println("** Valore di Default **");
+			soglia = .5;
+			input.nextLine();
+		}
+		System.out.println("Inserire numero di nodi da ricercare (Default: 10)");
+		try {
+			k = input.nextInt();
+		} catch (Exception e) {
+			System.out.println("** Valore di Default **");
+			k = 10;
+			input.nextLine();
+		}
+		System.out.println("Inserire numero di core (1|4) (Default: 4)");
+		try {
+			numeroCore = "" + input.nextInt();
+		} catch (Exception e) {
+			System.out.println("** Valore di Default **");
+			numeroCore = "4";
+			input.nextLine();
+		}
+		input.close();
+		System.out.println("Parametri scelti:");
+		System.out.println("Tipologia: " + tipologiaGrafo);
+		System.out.println("Numero di nodi k: " + k);
+		System.out.println("Soglia: " + soglia);
+		System.out.println("Numero di core: " + numeroCore);
+		mappaFile.put(1, "grande-abbastanza.txt");
+		mappaFile.put(2, "grande.txt");
+		mappaFile.put(3, "medio.txt");
+		mappaFile.put(4, "piccolo.txt");
+		System.setProperty("hadoop.home.dir", "C:\\Hadoop");
+		SparkConf conf = new SparkConf().setAppName("Advertisement").setMaster("local[*]")
+				.set("spark.driver.cores", numeroCore).set("spark.driver.memory", "4g");
+		jsc = new JavaSparkContext(conf);
+		jsc.setLogLevel("ERROR");
+
 	}
 }
